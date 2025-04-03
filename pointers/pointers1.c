44b16fe0188ac6fbbfbaef575c05abacc48e892b
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-04-03 10:58:08 -0300

    pointers

---
+++ pointers/pointers1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+int main(){
+    int value1=10;
+    int *value_pointer= &value1;
+    *value_pointer = 20;
+    printf("The value which value_pointer apoints %d\n",  *value_pointer);
+    printf("The memory point of value1 %p\n", value_pointer);
+
+
+}
