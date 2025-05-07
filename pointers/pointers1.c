d54cac9cb6197bad43acca09dcc30297b88c9daf
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-05-06 21:17:37 -0300

    bit manipulation

--- pointers/pointers1.c
+++ pointers/pointers1.c
@@ -7,7 +7,7 @@ int main(){
     int *value_pointer= &value1;
     *value_pointer = 20;
     printf("The value which value_pointer apoints %d\n",  *value_pointer);
-    printf("The memory point of value1 %p\n", value_pointer);
+    printf("The memory point of value1 is %p\n", value_pointer);
 
 
 }
