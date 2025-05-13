c1c2dce4797eb373bdb8107bd24852e8ea5070aa
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-05-13 20:44:28 -0300

    .

---
+++ memory/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define SIZE 2000000
+
+int main()
+{
+    double array[SIZE];
+    printf("tam. mem: %zu\n", sizeof(double)*SIZE);
+    for (int i = 0; i < SIZE; i++){
+        array[i] = i;
+
+}
+}
