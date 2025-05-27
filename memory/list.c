de3cfdaf09e8dc671d950f4fab464d261338cd14
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-05-27 20:32:22 -0300

    list menu

--- memory/list.c
+++ memory/list.c
@@ -92,13 +92,59 @@ int main()
     List lista;
     lista.head = lista.tail = NULL;
     lista.size = 0;
-
     add(&lista, 1);
     add(&lista, 2);
     add(&lista, 3);
     removeByValue(&lista,2);
     show(&lista);
     freeList(&lista);
+    int opcao, valor;
+
+        do {
+            printf("\nMenu:\n");
+            printf("1 - Adicionar no fim\n");
+            printf("2 - Adicionar no inicio\n");
+            printf("3 - Remover por valor\n");
+            printf("4 - Mostrar lista\n");
+            printf("5 - Sair\n");
+            printf("Escolha uma opcao: ");
+            scanf("%d", &opcao);
+
+            switch (opcao) {
+                case 1:
+                    printf("Digite o valor para adicionar no fim: ");
+                    scanf("%d", &valor);
+                    add(&lista, valor);
+                    break;
+
+                case 2:
+                    printf("Digite o valor para adicionar no inicio: ");
+                    scanf("%d", &valor);
+                    addStart(&lista, valor);
+                    break;
+
+                case 3:
+                    printf("Digite o valor para remover: ");
+                    scanf("%d", &valor);
+                    removeByValue(&lista, valor);
+                    break;
+
+                case 4:
+                    printf("Lista atual: ");
+                    show(&lista);
+                    break;
+
+                case 5:
+                    freeList(&lista);
+                    printf("Saindo...\n");
+                    break;
+
+                default:
+                    printf("Opcao invalida! Tente novamente.\n");
+            }
+        } while (opcao != 5);
+
+        return 0;
+    }
     // TODO: uma função para exibir essa lista
     // e não esquecer de liberar os nodos no final!!!
-}
